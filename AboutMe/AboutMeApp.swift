d380df7d178d5512f857241f40b0eec5480c2d64
Imarhia <imarhiaguobadia@gmail.com>
2024-07-26 13:29:31 +0100

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Scholar on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMe/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  AboutMe
+//
+//  Created by Scholar on 25/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color(.palegreen)
+                .ignoresSafeArea()
+            VStack{
+                HStack{
+                    VStack{
+                        Image("Baby pic")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(26)
+                            .padding([.leading, .bottom, .trailing])
+                        Spacer()
+                    }
+                    VStack{
+                                            
+                                            
+Image("photoToday")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.trailing, 0.0)
+                            .cornerRadius(26)
+                            .padding(.trailing)
+                                            Text("Imarhia")
+                                                
+                                        
+                                            Spacer()
+                                                
+                                            
+                            }
+                    
+
+                             
+                    }
+                }
+                
+                Spacer()
+            }
+        }
+    }
+
+
+#Preview {
+    ContentView()
+}
